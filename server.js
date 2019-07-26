0353baf801e63348180d13d4770d187b750cdcb1
TansuD <51340289+TansuD@users.noreply.github.com>
2019-07-25 20:56:47 -0700

    changes to html

--- server.js
+++ server.js
@@ -1,42 +1,21 @@
 var express = require("express");
 var path = require("path");
+var bodyParser = require("body-parser");
 
 var app = express();
 
 var PORT = process.env.PORT || 3000;
 
-app.get("/survey", function (req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
-});
+app.use(express.static(__dirname + "/app/css"));
 
-app.get("/api/friends", function (req, res) {
-    res.sendFile(path.join(__dirname, "package.json"));
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-app.post("/api/friends", function (req, res) {
-
-    var table = req.body;
-
-    table.routeName = table.name.replace(/\s+/g, "").toLowerCase();
-
-    console.log(table);
-
-    if (tables.length <= 4) {
-        tables.push(table);
-        console.log(table);
-        res.json({ res: true });
-    }
-    else {
-        waitlist.push(table);
-        res.json({ res: false });
-    }
-
-});
-
-
-
-
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function () {
-    console.log("Server listening on: http://localhost:" + PORT);
+    console.log("App listening on http://localhost:" + PORT);
 });
